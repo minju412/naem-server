99cc1e87df0ade4639c8c41ce374fb8913d83230
minju412 <ln8847@ajou.ac.kr>
2022-07-20 23:44:42 +0900

    fix: Member -> User

--- src/main/java/naem/server/service/AuthServiceImpl.java
+++ src/main/java/naem/server/service/AuthServiceImpl.java
@@ -1,9 +1,9 @@
 package naem.server.service;
 
 import lombok.extern.slf4j.Slf4j;
-import naem.server.domain.Member;
+import naem.server.domain.User;
 import naem.server.domain.Salt;
-import naem.server.repository.MemberRepository;
+import naem.server.repository.UserRepository;
 import naem.server.service.util.SaltUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -49,14 +49,14 @@ public class AuthServiceImpl implements AuthService {
     }
 
     @Override
-    public Member loginUser(String id, String password) throws ResponseStatusException {
+    public User loginUser(String id, String password) throws ResponseStatusException {
 
-        Member member = memberRepository.findByUsername(id);
-        if(member==null) throw new ResponseStatusException (HttpStatus.NOT_FOUND, "멤버가 조회되지 않음");
+        User user = userRepository.findByUsername(id);
+        if(user==null) throw new ResponseStatusException (HttpStatus.NOT_FOUND, "멤버가 조회되지 않음");
 
-        String salt = member.getSalt().getSalt();
+        String salt = user.getSalt().getSalt();
         password = saltUtil.encodePassword(salt,password);
-        if(!member.getPassword().equals(password))
+        if(!user.getPassword().equals(password))
             throw new ResponseStatusException (HttpStatus.UNAUTHORIZED, "비밀번호가 틀립니다.");
 
         return user;
