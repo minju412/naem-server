ae03acc29dff3f6496fca7b42746581feacfbec9
minju412 <ln8847@ajou.ac.kr>
2022-08-21 01:16:16 +0900

    fix: JwtAuthenticationEntryPoint 수정 - 우선은 리다이렉트 방식으로 다시 수정

--- src/main/java/naem/server/config/JwtAuthenticationEntryPoint.java
+++ src/main/java/naem/server/config/JwtAuthenticationEntryPoint.java
@@ -1,48 +1,27 @@
 package naem.server.config;
 
-import static naem.server.exception.ErrorCode.*;
-
 import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
-import naem.server.exception.ErrorCode;
 
 @Slf4j
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-    @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response,
-        AuthenticationException authException) throws IOException {
-
-        try {
-            sendErrprResponse(response, UNAUTHORIZED_USER);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+    public void commence(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        AuthenticationException ex
+    ) throws IOException {
+        log.error("UnAuthorized -- message : " + ex.getMessage());
+        response.sendRedirect("/auth/signIn"); // 로그인 페이지로 리다이렉트
     }
 
-    // 한글 출력을 위해 getWriter() 사용
-    private void sendErrprResponse(HttpServletResponse response, ErrorCode errorCode) throws
-        IOException,
-        JSONException {
-
-        response.setContentType("application/json;charset=UTF-8");
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-
-        JSONObject responseJson = new JSONObject();
-        responseJson.put("error", errorCode.getHttpStatus());
-        responseJson.put("message", errorCode.getMessage());
-
-        response.getWriter().print(responseJson);
-    }
 }
